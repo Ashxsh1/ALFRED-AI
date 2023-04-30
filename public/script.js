61239d4c008f23dde7d3e2cae80d89e417639836
Your Name <you@example.com>
2023-05-01 01:12:47 +0530

    added typing animation in the starting

--- public/script.js
+++ public/script.js
@@ -1,8 +1,9 @@
 $(document).ready(() => {
   const messageList = $('.message-list');
   const messageForm = $('.message-form');
+  const messageHistory = [];
 
-  messageForm.submit((event) => {
+  messageForm.submit(async (event) => {
     event.preventDefault();
     const messageInput = $('input[name="message"]');
     const message = messageInput.val().trim();
@@ -11,26 +12,33 @@ $(document).ready(() => {
     }
     messageInput.val('');
     addMessage('user', message);
-    sendMessage(message);
+    const newHistory = await sendMessage(message, messageHistory);
+    messageHistory.push(...newHistory);
   });
+  
 
-  function addMessage(sender, message) {
-    const li = $('<li></li>');
-    li.addClass(`${sender}-message`);
-    li.text(message);
-    messageList.append(li);
-    messageList.scrollTop(messageList.prop('scrollHeight'));
-  }
 
-  async function sendMessage(message) {
+function addMessage(sender, message) {
+  const li = $('<li></li>');
+  li.addClass(`${sender}-message`);
+  li.text(message);
+  messageList.append(li);
+  messageList.scrollTop(messageList.prop('scrollHeight'));
+  messageHistory.push({ sender, message });
+}
+
+  async function sendMessage(message, history) {
     try {
-      const response = await axios.post('/chat', { message });
-      const { message: reply } = response.data;
+      const response = await axios.post('/chat', { message, history });
+      const { message: reply, newHistory } = response.data;
       addMessage('bot', reply);
+      return newHistory;
     } catch (error) {
       console.error(error);
       addMessage('bot', 'Sorry, I am not able to process your request.');
+      return history;
     }
   }
+  
   
 });
